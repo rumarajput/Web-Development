60992e951a64c6f92900fd849bf32b86759ef9e3
RumaRajput <rajputruma792@gmail.com>
2023-06-29 15:32:59 +0530

    latest changes

--- hello.js
+++ hello.js
@@ -2,5 +2,11 @@ console.log("hello wold");
 console.log(12);
 console.log(15.2);
 console.log(-12);
-console.log(126/4);
+console.log(126/4); 
+
+
+
+//successfully connected git + github + vsCode together
+//now i can commit and my changes to main branch of my reposetory
+//also i can dop the push pull from any branch that is present under my repositiry
 
